2b1ed331c7117c056a402b2ef5a740d6c0d3f91e
Daniel Passos <daniel.hpassos@hotmail.com>
2015-09-01 16:02:42 -0300

    Revert "Revert "Criação Cliente e Repository""
    This reverts commit 449a3ac6fd2aa6826fc341df02bcdf84fd2b7169.

---
+++ WebAPIPortfolioDB/Models/Entities/Cliente.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAPIPortfolioDB.Models.Entities
+{
+    public class Cliente
+    {
+        public int ClienteId { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Login { get; set; }
+        public string Senha { get; set; }
+    }
+}
---
+++ WebAPIPortfolioDB/Models/Interfaces/IRepositoryBase.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAPIPortfolioDB.Models.Interfaces
+{
+    public interface IRepositoryBase<TEntity> where TEntity : class
+    {
+        void Add(TEntity obj);
+
+        List<TEntity> Listar();
+
+        TEntity Buscar(int id);
+
+        void Update(TEntity obj);
+
+        void Delete(TEntity obj);
+    }
+}
